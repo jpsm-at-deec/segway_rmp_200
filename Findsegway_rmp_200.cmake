63781d49a9e3184da8fba7f3bad318bcf4de8ac3
fherrero <fherrero@iri.upc.edu>
2020-05-13 13:01:16 +0200

    Update Find cmake with correct paths and path order

--- Findsegway_rmp_200.cmake
+++ Findsegway_rmp_200.cmake
@@ -2,12 +2,12 @@ SET(name segway_rmp_200)
 #edit the following line to add the librarie's header files
 SET(header_files segway_rmp200.h segway_rmp200_exceptions.h)
 
-FIND_PATH(${name}_INCLUDE_DIRS ${header_files} /usr/include/iri/${name} /usr/local/include/iri/${name})
+FIND_PATH(${name}_INCLUDE_DIRS ${header_files}.h /usr/local/include/iri/${name} /usr/include/iri/${name})
 
 FIND_LIBRARY(${name}_LIBRARIES
     NAMES ${name}
-    PATHS /usr/lib /usr/lib/iri/${name} /usr/local/lib /usr/local/lib/iri/${name})
-
+    PATHS /usr/local/lib/iri/${name} /usr/lib/iri/${name})
+    
 SET(${name}_INCLUDE_DIR ${${name}_INCLUDE_DIRS})
 SET(${name}_LIBRARY     ${${name}_LIBRARIES})
 
@@ -23,4 +23,4 @@ ELSE (${name}_FOUND)
    IF (${name}_FIND_REQUIRED)
       MESSAGE(FATAL_ERROR "Could not find ${name}")
    ENDIF (${name}_FIND_REQUIRED)
-ENDIF (${name}_FOUND)        
+ENDIF (${name}_FOUND)
